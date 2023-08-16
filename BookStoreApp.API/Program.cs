f61643f27591098563b18b1ed02d820076caa04c
asgharm <asgharm@enterprise.trin.net>
2023-08-15 20:30:36 -0500

    jwt added

--- BookStoreApp.API/Program.cs
+++ BookStoreApp.API/Program.cs
@@ -1,8 +1,11 @@
 using BookStoreApp.API.Configurations;
 using BookStoreApp.API.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Serilog;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -31,6 +34,25 @@ builder.Services.AddCors(options => {
         .AllowAnyOrigin());
 });
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        ValidateIssuer = true,
+        ValidateAudience = true, 
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero, 
+        ValidIssuer = builder.Configuration["JwtSettings: Issuer"],
+        ValidAudience = builder.Configuration["JwtSettings: Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Key"]))
+
+    }
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -43,7 +65,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseCors("AllowAll");
-app.UseRouting();
+// app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
